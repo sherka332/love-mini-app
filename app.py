b413f9fbd51b3dd9060b095a0b5b29e5577e2a61
sherka332 <nurullayevsherbek9@gmail.com>
2026-09-07 21:48:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+from flask import Flask, send_from_directory
+import os
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return send_from_directory(".", "index.html")
+
+@app.route("/api/health")
+def health():
+    return {
+        "status": "ok",
+        "message": "Love Mini App API ishlayapti ❤️"
+    }
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
